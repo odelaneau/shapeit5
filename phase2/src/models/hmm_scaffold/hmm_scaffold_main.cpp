705d958ef99c1182d1edaafcc88cedfaadfa2966
odelaneau <olivier.delaneau@gmail.com>
2022-03-29 05:59:31 +0200

    AVX2 code for phase2

--- phase2/src/models/hmm_scaffold/hmm_scaffold_main.cpp
+++ phase2/src/models/hmm_scaffold/hmm_scaffold_main.cpp
@@ -23,13 +23,13 @@
 
 hmm_scaffold::hmm_scaffold(unsigned int _hap, variant_map & _V, genotype_set & _G, conditioning_set & _C, hmm_parameters & _M) : V(_V), G(_G), C(_C), M(_M){
 	hap = _hap;
-	emit[0] = 1.0f; emit[1] = M.ed/M.ee;
+	match_prob[0] = 1.0f; match_prob[1] = M.ed/M.ee;
 
 	unsigned max_nstates = 0;
 	for (int h = 0 ; h < C.n_haplotypes ; h ++) if (C.indexes_pbwt_neighbour[h].size() > max_nstates) max_nstates = C.indexes_pbwt_neighbour[h].size();
 
-	alpha = vector < vector < float > > (C.n_scaffold_variants, vector < float > (max_nstates, 0.0f));
-	beta = vector < float > (max_nstates, 1.0f);
+	alpha = vector < aligned_vector32 < float > > (C.n_scaffold_variants, aligned_vector32 < float > (max_nstates, 0.0f));
+	beta = aligned_vector32 < float > (max_nstates, 1.0f);
 
 	nstates = C.indexes_pbwt_neighbour[hap].size();
 	Hhap.subset(C.Hhap, C.indexes_pbwt_neighbour[hap]);
@@ -45,52 +45,128 @@ hmm_scaffold::~hmm_scaffold() {
 
 void hmm_scaffold::forward() {
 	float sum;
+	const unsigned int nstatesMD8 = (nstates / 8) * 8;
+	const __m256i _vshift_count = _mm256_set_epi32(31,30,29,28,27,26,25,24);
 	for (int vs = 0 ; vs < C.n_scaffold_variants ; vs ++) {
-		if (!vs) fill(alpha[vs].begin(), alpha[vs].begin() + nstates, 1.0f / nstates);
-		else {
-			float f0 = M.t[vs-1] / nstates;
-			float  f1 = M.nt[vs-1] / sum;
-			for (int k = 0 ; k < nstates ; k ++) alpha[vs][k] = alpha[vs-1][k] * f1 + f0;
-		}
-		sum = 0.0f;
-		for (int k = 0 ; k < nstates ; k ++) {
-			alpha[vs][k] *= emit[C.Hhap.get(hap, vs) != Hvar.get(vs, k)];
-			sum += alpha[vs][k];
+		const std::array<float,2> emit = {match_prob[C.Hhap.get(hap, vs)], match_prob[1-C.Hhap.get(hap, vs)]};
+		const __m256 _emit0 = _mm256_set1_ps(emit[0]);
+		const __m256 _emit1 = _mm256_set1_ps(emit[1]);
+
+		if (!vs) {
+			const float f0 = 1.0f / nstates;
+			const __m256 _f0 = _mm256_set1_ps(f0);
+			__m256 _sum = _mm256_set1_ps(0.0f);
+			int offset = 0;
+			for (int k = 0 ; k < nstatesMD8 ; k += 8) {
+				const __m256i _mask = _mm256_sllv_epi32(_mm256_set1_epi32((unsigned int )Hvar.getByte(vs, k)), _vshift_count);
+				const __m256 _emiss = _mm256_blendv_ps (_emit0, _emit1, _mm256_castsi256_ps(_mask));
+				const __m256 _prob_curr = _mm256_mul_ps(_emiss, _f0);
+				_sum = _mm256_add_ps(_sum, _prob_curr);
+				_mm256_store_ps(&alpha[vs][k], _prob_curr);
+				offset += 8;
+			}
+			sum = (offset > 0)?horizontal_add(_sum):0.0f;
+			for (; offset < nstates ; offset ++) {
+				alpha[vs][offset] = f0 * emit[Hvar.get(vs, offset)];
+				sum += alpha[vs][offset];
+			}
+		} else {
+			const float f0 = M.t[vs-1] / nstates;
+			const float f1 = M.nt[vs-1] / sum;
+			const __m256 _f0 = _mm256_set1_ps(f0);
+			const __m256 _f1 = _mm256_set1_ps(f1);
+			__m256 _sum = _mm256_set1_ps(0.0f);
+			int offset = 0;
+			for (int k = 0 ; k < nstatesMD8 ; k += 8) {
+				const __m256i _mask = _mm256_sllv_epi32(_mm256_set1_epi32((unsigned int )Hvar.getByte(vs, k)), _vshift_count);
+				const __m256 _emiss = _mm256_blendv_ps (_emit0, _emit1, _mm256_castsi256_ps(_mask));
+				const __m256 _prob_prev = _mm256_load_ps(&alpha[vs-1][k]);
+				const __m256 _prob_temp = _mm256_fmadd_ps(_prob_prev, _f1, _f0);
+				const __m256 _prob_curr = _mm256_mul_ps(_prob_temp, _emiss);
+				_sum = _mm256_add_ps(_sum, _prob_curr);
+				_mm256_store_ps(&alpha[vs][k], _prob_curr);
+				offset += 8;
+			}
+			sum = (offset > 0)?horizontal_add(_sum):0.0f;
+			for (; offset < nstates ; offset ++) {
+				alpha[vs][offset] = (alpha[vs-1][offset]*f1+f0)*emit[Hvar.get(vs, offset)];
+				sum += alpha[vs][offset];
+			}
 		}
 	}
 }
 
 void hmm_scaffold::backward(vector < bool > & cevents, vector < state > & cstates) {
 	float sum = 0.0f, scale = 0.0f, threshold = 1.0f / (nstates+1);
-	vector < float > alphaXbeta_curr = vector < float >(nstates, 0.0f);
-	vector < float > alphaXbeta_prev = vector < float >(nstates, 0.0f);
+	const unsigned int nstatesMD8 = (nstates / 8) * 8;
+	const __m256i _vshift_count = _mm256_set_epi32(31,30,29,28,27,26,25,24);
+	aligned_vector32 < float > alphaXbeta_curr = aligned_vector32 < float >(nstates, 0.0f);
+	aligned_vector32 < float > alphaXbeta_prev = aligned_vector32 < float >(nstates, 0.0f);
 
 	cstates.clear();
-	fill (beta.begin(), beta.end(), 1.0f / nstates);
+	//fill (beta.begin(), beta.end(), 1.0f / nstates);
 
 	for (int vs = C.n_scaffold_variants - 1 ; vs >= 0 ; vs --) {
 
+		//
+		const std::array<float,2> emit = {match_prob[C.Hhap.get(hap, vs)], match_prob[1-C.Hhap.get(hap, vs)]};
+		const __m256 _emit0 = _mm256_set1_ps(emit[0]);
+		const __m256 _emit1 = _mm256_set1_ps(emit[1]);
+
 		//Transitions
-		if (vs < C.n_scaffold_variants - 1) {
-			float f0 = M.t[vs] / nstates;
-			float  f1 = M.nt[vs] / sum;
-			for (int k = 0 ; k < nstates ; k ++) beta[k] = beta[k] * f1 + f0;
+		if (vs == C.n_scaffold_variants - 1) fill (beta.begin(), beta.end(), 1.0f / nstates);
+		else {
+			const float f0 = M.t[vs] / nstates;
+			const float f1 = M.nt[vs] / sum;
+			const __m256 _f0 = _mm256_set1_ps(f0);
+			const __m256 _f1 = _mm256_set1_ps(f1);
+			int offset = 0;
+			for (int k = 0 ; k < nstatesMD8 ; k += 8) {
+				const __m256 _prob_prev = _mm256_load_ps(&beta[k]);
+				const __m256 _prob_curr = _mm256_fmadd_ps(_prob_prev, _f1, _f0);
+				_mm256_store_ps(&beta[k], _prob_curr);
+				offset += 8;
+			}
+			for (; offset < nstates ; offset ++) beta[offset] = (beta[offset]*f1+f0);
 		}
 
 		//Products
-		scale = 0.0f;
-		for (int k = 0 ; k < nstates ; k ++) {
-			alphaXbeta_curr[k] = alpha[vs][k] * beta[k];
-			scale += alphaXbeta_curr[k];
+		__m256 _scale = _mm256_set1_ps(0.0f);
+		int offset = 0;
+		for (int k = 0 ; k < nstatesMD8 ; k += 8) {
+			const __m256 _prob_temp = _mm256_mul_ps(_mm256_load_ps(&alpha[vs][k]), _mm256_load_ps(&beta[k]));
+			_mm256_store_ps(&alphaXbeta_curr[k], _prob_temp);
+			_scale = _mm256_add_ps(_scale, _prob_temp);
+		}
+		scale = (offset > 0)?horizontal_add(_scale):0.0f;
+		for (; offset < nstates ; offset ++) {
+			alphaXbeta_curr[offset] = alpha[vs][offset] * beta[offset];
+			scale += alphaXbeta_curr[offset];
 		}
 		scale = 1.0f / scale;
-		for (int k = 0 ; k < nstates ; k ++) alphaXbeta_curr[k] *= scale;
+		_scale = _mm256_set1_ps(scale);
+		offset = 0;
+		for (int k = 0 ; k < nstatesMD8 ; k += 8) {
+			_mm256_store_ps(&alphaXbeta_curr[k], _mm256_mul_ps(_mm256_load_ps(&alphaXbeta_curr[k]), _scale));
+		}
+		for (; offset < nstates ; offset ++) alphaXbeta_curr[offset] *= scale;
 
 		//Emission
-		sum = 0.0f;
-		for (int k = 0 ; k < nstates ; k ++) {
-			beta[k] *= emit[C.Hhap.get(hap, vs) != Hvar.get(vs, k)];
-			sum += beta[k];
+		__m256 _sum = _mm256_set1_ps(0.0f);
+		offset = 0;
+		for (int k = 0 ; k < nstatesMD8 ; k += 8) {
+			const __m256i _mask = _mm256_sllv_epi32(_mm256_set1_epi32((unsigned int )Hvar.getByte(vs, k)), _vshift_count);
+			const __m256 _emiss = _mm256_blendv_ps (_emit0, _emit1, _mm256_castsi256_ps(_mask));
+			const __m256 _prob_prev = _mm256_load_ps(&beta[k]);
+			const __m256 _prob_curr = _mm256_mul_ps(_prob_prev, _emiss);
+			_sum = _mm256_add_ps(_sum, _prob_curr);
+			_mm256_store_ps(&beta[k], _prob_curr);
+			offset += 8;
+		}
+		sum = (offset > 0)?horizontal_add(_sum):0.0f;
+		for (; offset < nstates ; offset ++) {
+			beta[offset] *= emit[Hvar.get(vs, offset)];
+			sum += beta[offset];
 		}
 
 		//Storage
