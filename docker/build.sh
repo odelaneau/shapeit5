249dd562d7cadcfaa2b61f3401d406114c9c6c24
odelaneau <olivier.delaneau@gmail.com>
2022-04-17 17:28:40 +0200

    Add flip calculations into switch

--- docker/build.sh
+++ docker/build.sh
@@ -4,21 +4,25 @@ make clean
 make -j 8 static_exe
 cp bin/SHAPEIT5_phase1_static ../static_bins/.
 
+#Compile phase 2
 cd ../phase2/
 make clean
 make -j 8 static_exe
 cp bin/SHAPEIT5_phase2_static ../static_bins/.
 
+#Compile switch
 cd ../switch/
 make clean
 make -j 8 static_exe
 cp bin/SHAPEIT5_switch_static ../static_bins/.
 
+#Compile ligate
 cd ../ligate/
 make clean
 make -j 8 static_exe
 cp bin/SHAPEIT5_ligate_static ../static_bins/.
 
+#Buld docker image
 cd ../docker/
 cp ../static_bins/SHAPEIT5* ressources/.
 docker build -t shapeit5_0.0.1 -f Dockerfile .
--- docker/upload.sh
+++ docker/upload.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
+#Upload docker image onto DNA Nexus
 dx rm "/docker/shapeit5_0.0.1.tar.gz" -f
 dx upload shapeit5_0.0.1.tar.gz --path "/docker/"
