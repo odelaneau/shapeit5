8212e62c30c18be66d8b01a6e89bd6a6f97da150
odelaneau <olivier.delaneau@gmail.com>
2023-03-13 09:10:15 +0100

    Clean-up

--- tasks/phasingUKB/step2_wgs/v0/step5_runshapeit5_phase2.sh
+++ tasks/phasingUKB/step2_wgs/v0/step5_runshapeit5_phase2.sh
@@ -27,20 +27,21 @@ done
 
 DOCKER=shapeit5.test.tar.gz
 
+
 while read LINE; do
 	SREG=$(echo $LINE | awk '{ print $3; }')
 	IREG=$(echo $LINE | awk '{ print $4; }')
 	
 	PCF=/mnt/project/data/ukb_wgs/unphased/qc/ukb23352_c20_qc_v1.bcf
 	PED=/mnt/project/Phasing/PhasingSNParray/step1_dataqc/samples.families.caucasian.txt
-	SCA=/mnt/project/Phasing/PhasingWGS/step9_production/ukb23352_c20_qc_v1.common.phased.bcf
+	#SCA=/mnt/project/Phasing/PhasingWGS/step9_production/ukb23352_c20_qc_v1.common.phased.bcf
+	SCA=/mnt/project/Phasing/PhasingWGS/step4_runshapeit_phase1/N$N\.test/benchmark_ukb23352_c20_qc_v1.subset.N147754.fullchr.shapeit5.ped.bcf
 	
-	OUT=benchmark_ukb23352_c20_qc_v1.subset.N$N\.$SREG\.shapeit5.ped2.bcf
-	TIM=benchmark_ukb23352_c20_qc_v1.subset.N$N\.$SREG\.shapeit5.ped2.time
-	LOG=benchmark_ukb23352_c20_qc_v1.subset.N$N\.$SREG\.shapeit5.ped2.log
+	OUT=benchmark_ukb23352_c20_qc_v1.subset.N147754.$SREG\.shapeit5.ped.bcf
+	TIM=benchmark_ukb23352_c20_qc_v1.subset.N147754.$SREG\.shapeit5.ped.time
+	LOG=benchmark_ukb23352_c20_qc_v1.subset.N147754.$SREG\.shapeit5.ped.log
 	
 	dx run app-swiss-army-knife -iimage_file="/docker/$DOCKER" --folder="/Phasing/PhasingWGS/step5_runshapeit_phase2/N147754.test/" -icmd="/usr/bin/time -vo $TIM SHAPEIT5_phase_rare_static --input-plain $PCF --scaffold $SCA --map $MAP --output-plain $OUT --log $LOG --scaffold-region $SREG --input-region $IREG --thread 32 --pedigree $PED" --tag benchWGS --tag shapeit5 --tag phase2 --tag $SREG --instance-type mem3_ssd1_v2_x32 --priority normal --name benchWGS_shapeit5_phase2a_$SREG -y
 
 done < /home/olivier/Dropbox/Repository/shapeit5/tasks/phasingUKB/step2_wgs/v0/step2_splitchunks/chr20.size4Mb.txt
-
 
