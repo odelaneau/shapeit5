643878bed97c75427cfa76e1d405a1fd15703691
odelaneau <olivier.delaneau@gmail.com>
2023-04-08 12:30:37 +0200

    Adding a tool to simulate chrX and family datasets + reforge of all the example datasets

---
+++ test/scripts/phase.wgs.haploid.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+HAP=info/target.haploid.txt
+CRD=info/chunks.coordinates.txt
+LST=tmp/chunks.files.txt
+rm $LST
+
+#PHASE SCAFFOLD SITES WITH MAF >=0.1%
+../phase_common/bin/phase_common --input wgs/target.haploid.bcf --filter-maf 0.001 --haploids $HAP --region 1 --map info/chr1.gmap.gz --output tmp/target.scaffold.bcf --thread 8
+
+#PHASE RARE SITES ONTO SCAFFOLD
+while read LINE; do
+	CHK=$(echo $LINE | awk '{ print $1; }')
+	SRG=$(echo $LINE | awk '{ print $3; }')
+	IRG=$(echo $LINE | awk '{ print $4; }')
+
+	OUT=tmp/target.phased.chunk$CHK\.bcf
+	../phase_rare/bin/phase_rare --input wgs/target.family.bcf --scaffold tmp/target.scaffold.bcf --haploids $HAP --map info/chr1.gmap.gz --input-region $IRG --scaffold-region $SRG --output $OUT  --thread 8
+
+	echo $OUT >> $LST
+done < $CRD
+bcftools concat -n -Ob -o target.phased.bcf -f $LST
+bcftools index target.phased.bcf
+
+
